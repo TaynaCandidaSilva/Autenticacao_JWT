38a281328be50c277252db9fd04d83aa3d357ab8
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-12-26 10:56:06 -0300

    Adicionando .env no projeto

--- src/drivers/jwt_handler.py
+++ src/drivers/jwt_handler.py
@@ -1,6 +1,7 @@
 import jwt
 from typing import Dict
 from datetime import datetime, timedelta, timezone
+from src.configs.jwt_configs import jwt_infos
 
 
 class JwtHandler:
@@ -8,14 +9,17 @@ class JwtHandler:
     def create_jwt_token(self, body: Dict = {}) -> str:
         token = jwt.encode(
             payload={
-                "exp": datetime.now(timezone.utc) + timedelta(minutes=1),
+                "exp": datetime.now(timezone.utc)
+                + timedelta(hours=int(jwt_infos["JWT_HOURS"])),
                 **body,
             },
-            key="minhaChave",
-            algorithm="HS256",
+            key=jwt_infos["KEY"],
+            algorithm=jwt_infos["ALGORITHM"],
         )
         return token
 
     def decode_jwt_token(self, token: str) -> Dict:
-        token_information = jwt.decode(token, key="minhaChave", algorithms="HS256")
+        token_information = jwt.decode(
+            token, key=jwt_infos["KEY"], algorithms=jwt_infos["ALGORITHM"]
+        )
         return token_information
