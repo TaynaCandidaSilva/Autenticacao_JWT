38b4d1e17034f8e78bc503ac34764b9b75b97f05
TaynaCandidaSilva~ <tayna2507@gmail.com>
2024-12-30 14:23:52 -0300

    Implementando Interfaces em COntrollers

---
+++ src/controllers/interfaces/user_register.py
@@ -0,0 +1,9 @@
+from typing import Dict
+from abc import ABC, abstractmethod
+
+
+class UserRegisterInterface(ABC):
+
+    @abstractmethod
+    def registry(self, username: str, password: str) -> Dict:
+        pass
