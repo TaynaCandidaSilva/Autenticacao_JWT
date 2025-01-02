d4c3cc841fc947ba2ea91e6944c0fe2823914e10
TaynaCandidaSilva~ <tayna2507@gmail.com>
2025-01-02 10:35:30 -0300

    Implementando rota com logica de controller

--- src/controllers/user_register.py
+++ src/controllers/user_register.py
@@ -10,16 +10,23 @@ class UserRegister(UserRegisterInterface):
         self.__password_handler = PasswordHandler()
 
     def registry(self, username: str, password: str) -> Dict:
-        hashed_password = self.__create_hash_password(password)
-        self.__register_new_user(username, hashed_password)
+        hashed_password = self.__create_hashed_password(password)
+
+        self.__registry_new_user(username, hashed_password)
+
         return self.__format_response(username)
 
-    def __create_hash_password(self, password: str) -> str:
+    def __create_hashed_password(self, password: str) -> str:
         hashed_password = self.__password_handler.encrypt_password(password)
+
         return hashed_password
 
-    def __register_new_user(self, username: str, hashed_password: str) -> None:
+    def __registry_new_user(self, username: str, hashed_password: str) -> None:
         self.__user_repository.registry_user(username, hashed_password)
 
-    def __format_response(self, username) -> Dict:
-        return {"type": "User", "count": 1, "username": username}
+    def __format_response(self, username: str) -> Dict:
+        return {
+            "type": "User",
+            "count": 1,
+            "username": username,
+        }
--- src/views/user_register_view.py
+++ src/views/user_register_view.py
@@ -6,14 +6,14 @@ from .interfaces.view_interface import ViewInterface
 
 class UserRegisterView(ViewInterface):
     def __init__(self, controller: UserRegisterInterface) -> None:
-        self.controller = controller
+        self.__controller = controller
 
     def handle(self, http_request: HttpRequest) -> HttpResponse:
         username = http_request.body.get("username")
         password = http_request.body.get("password")
         self.__validate_inputs(username, password)
 
-        response = self.controller.registry(username, password)
+        response = self.__controller.registry(username, password)
         return HttpResponse(body={"data": response}, status_code=201)
 
     def __validate_inputs(self, username: any, password: any) -> None:
@@ -23,4 +23,4 @@ class UserRegisterView(ViewInterface):
             or not isinstance(username, str)
             or not isinstance(password, str)
         ):
-            raise Exception("Invalid input")
+            raise Exception("Invalid Input")
